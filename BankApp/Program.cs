433c3445a31fcb41fe917e5ae010a5e2789c9c62
emre381 <cihatemre381@gmail.com>
2024-09-27 19:14:11 +0300

    first commit

---
+++ BankApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp
+{
+    // deposit para yatırmak withdraw
+     class Program
+    {
+        class BankAccount
+        {
+            private int accountBlance;
+            public void withDraw(int amount)
+            {
+                accountBlance += amount;
+            }
+            public void deposit(int amount)
+            {
+                accountBlance -= amount;
+            }
+            public int accountBlanceCheck()
+            {
+                return accountBlance;
+            }
+        }
+        static void Main(string[] args)
+        {
+            BankAccount b1 = new BankAccount();
+            Console.WriteLine(b1.accountBlanceCheck());
+
+            b1.withDraw(1500);
+            Console.WriteLine("Amount in your bank account:" + b1.accountBlanceCheck());
+            b1.deposit(100);
+            Console.WriteLine("Amount in your bank account:" + b1.accountBlanceCheck());
+            Console.ReadLine();
+
+
+        }
+    }
+}
